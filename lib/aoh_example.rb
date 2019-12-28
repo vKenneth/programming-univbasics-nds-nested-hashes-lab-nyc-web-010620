58864bbde5b9f2bfb76072ca90ab475ba6dab2de
K e n V <kenneth_valdivia@yahoo.com>
2019-12-28 21:57:44 +0000

    Done.

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -23,3 +23,23 @@ def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
+
+     [
+     DON_G,
+     JOELLE_VD,
+     PAT_M,
+     KATE_G,
+     BRUCE_G,
+     ]
+     
+     [
+ { name:"Don Gately", occupation: "Live-in Staff" },
+ { name:"Joelle van Dyne", occupation: "Radio Personality" },
+ { name:"Pat Monteseian", occupation: "Staff" },
+ { name:"Kate Gompert", occupation: "None" },
+ { name:"Bruce Green", occupation: "Fan of Mildred"}
+ 
+ def aoh_update(aoh, row, key, new_value)
+   aoh[row][key] = new_value
+   return aoh
+ end
